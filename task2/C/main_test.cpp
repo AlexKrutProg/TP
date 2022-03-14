2c76ab21372e99b8898d783bbe710fa74bd665f8
alexkrut <a.d.ivanov@phystech.edu>
2022-03-14 18:31:42 +0300

    Final

--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include "main_tests.cpp"
+//#include "main_tests.cpp"
 
-int main(int argc, int *argv[]) {
+int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- task2/C/main_tests.cpp
+++ task2/C/main_tests.cpp
@@ -1,10 +1,13 @@
 #include "A/index.h"
 #include "B/lib.h"
+#include <gtest/gtest.h>
 
-TEST(arr, test1) {
-    ASSERT_EQ(arr[0], 1)
-    ASSERT_EQ(arr[4], 5)
+TEST(ATest, test1) {
+    ASSERT_EQ(arr[0], 1);
+    ASSERT_EQ(arr[4], 5);
 }
 
-TEST(create_love, test2) {
-    ASSERT_EQ(create_love("Sonechka"), "Sonechka, I love you!")
+TEST(BTest, test2) {
+    ASSERT_EQ(create_love("Sonechka"), "Sonechka, I love you!");
+}
+
