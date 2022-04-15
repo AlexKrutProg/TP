577fca9fe794434510c7a4b25cb25abe4c1bfb71
alexkrut <a.d.ivanov@phystech.edu>
2022-04-15 18:38:44 +0300

    First

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -1,8 +1,23 @@
-//
-// Created by akhtyamovpavel on 5/1/20.
-//
-
 
 #include "TreeTestCase.h"
 #include "Tree.h"
+
+TEST(TreeTestCase, NotExist) {
+    ASSERT_THROW(GetTree("longLongAnyWay", true), std::invalid_argument);
+}
+
+TEST(TreeTestCase, OperatorEq) {
+    ASSERT_EQ(GetTree(".", false), GetTree(".", false));
+}
+
+TEST(TreeTestCase, NotDirectory) {
+    ASSERT_THROW(GetTree("./Tree.cpp", false), std::invalid_argument);
+}
+
+
+TEST(TreeTestCase, Filter) {
+    ASSERT_NO_FATAL_FAILURE(FilterEmptyNodes(GetTree(".", false), "."));
+}
+
+
 
