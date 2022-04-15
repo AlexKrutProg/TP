307360ad25add6eddfbb1620c1841abaebf487cd
alexkrut <a.d.ivanov@phystech.edu>
2022-04-15 19:24:31 +0300

    Second

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -1,23 +1,40 @@
 
 #include "TreeTestCase.h"
 #include "Tree.h"
+#include <filesystem>
+
 
 TEST(TreeTestCase, NotExist) {
     ASSERT_THROW(GetTree("longLongAnyWay", true), std::invalid_argument);
 }
 
 TEST(TreeTestCase, OperatorEq) {
-    ASSERT_EQ(GetTree(".", false), GetTree(".", false));
+    ASSERT_EQ(GetTree(".", true), GetTree(".", true));
 }
 
 TEST(TreeTestCase, NotDirectory) {
     ASSERT_THROW(GetTree("./Tree.cpp", false), std::invalid_argument);
 }
 
+TEST(TreeTestCase, NotDirectorySec) {
+    namespace fs = std::filesystem;
+    using std::filesystem::is_directory;
+    for (const auto & entry : fs::directory_iterator(".")) {
+        if (!is_directory(entry.path())) {
+            ASSERT_THROW(GetTree(entry.path(), false), std::invalid_argument);
+            break;
+        }
+    }
+}
 
-TEST(TreeTestCase, Filter) {
+TEST(TreeTestCase, Last) {
     ASSERT_NO_FATAL_FAILURE(FilterEmptyNodes(GetTree(".", false), "."));
 }
+
+
+
+
+
 
 
 
