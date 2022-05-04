44df16eeec58973edd162a23b28797a028a0cd6e
AlexKrut <a.d.ivanov@phystech.edu>
2022-05-04 16:02:14 +0300

    Update run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#цикл считывает флаги из командной строки
 for i in 1 2 3 4
 do
 case "$1" in
@@ -12,41 +13,43 @@ shift
 shift
 done
 
+#находим пути до необходимых папок
 backup_folder=$( readlink -f "$backup_folder" )
 input_folder=$( readlink -f "$input_folder" )
 
 
 init=$( pwd )
 
+#во временной папке, чтобы не засорят компьютер, создаем файл, в который будем записывать пути до нужных папок 
 touch /tmp/paths.txt
 
+#записываем пути до папок, чтобы создать туже архитектуру
 find "${input_folder}" -type d -printf "%P\0" > /tmp/paths.txt
-
-# OK so far
-# exit 1
 
 mkdir "${backup_folder}"
 cd "${backup_folder}"
 
+#создаем все папки в backup папке
 xargs --null mkdir < /tmp/paths.txt &> /dev/null
 
 cd "$init"
-#cd ${input_folder}
-#exit 1
 
+#снова во временной папке создаем файл для путей до всех нужных файлов
 touch /tmp/paths_files.txt
 
+#записываем пути до файлов только с необходимым расширением 
 find "${input_folder}" -type f -name "*.${extension}" -printf "%P\n" > /tmp/paths_files.txt
 
 cd "$init"
 cd "$input_folder"
 
+#циклом копируем все файлы, пути которых мы сохранили, в папку восстановления
 while IFS= read -r path; do
     cp "${path}" "${backup_folder}/${path}"
 done < /tmp/paths_files.txt
 
 cd "$init"
-
+#создаем архив из папки восстановления
 tar zcf "${backup_archive_name}" "${backup_folder}" &> /dev/null
 
 echo done
